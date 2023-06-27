14c7361eb39a8b9258765ca665d929665866e2ab
Noah Overcash <novercash@ebsco.com>
2023-06-26 23:11:09 -0500

    Use custom benchmarking code (sorry, benny!)

--- README.md
+++ README.md
@@ -4,10 +4,20 @@ This script runs a benchmark against a local MinIO server to compare the speeds
 of uploads with and without multipart upload; additionally, it will compare the
 speeds of multipart uploads with different chunk sizes.
 
+## Requirements
+
+- S3-like storage and credentials
+- At least 15 GB of allocated storage in the S3 instance
+- Approximately eight minutes to let it cook (depending on your bandwidth/server)
+
 ## Usage
 
 First, generate access keys for your MinIO instance. Then, run:
 
 ```sh
-ACCESS_KEY=ABCDEF SECRET_KEY=123456 yarn benchmark
+ACCESS_KEY=ABCDEF SECRET_KEY=123456 ENDPOINT=http://localhost:9000 yarn benchmark
 ```
+
+If these are not specified, the script will default to `http://localhost:9000` with credentials `minioadmin`:`minioadmin`
+
+\*\*Note: this will probably take at least 15 GB of storage on your device/S3 instance.
--- index.ts
+++ index.ts
@@ -21,7 +21,7 @@ const s3 = new S3Client({
     accessKeyId: process.env.ACCESS_KEY ?? 'minioadmin',
     secretAccessKey: process.env.SECRET_KEY ?? 'minioadmin',
   },
-  endpoint: 'http://localhost:9000',
+  endpoint: process.env.ENDPOINT ?? 'http://localhost:9000',
   forcePathStyle: true,
   region: 'us-east-1',
 });
@@ -121,161 +121,9 @@ function sizeToBytes(size: string): number {
   throw new Error(`Invalid size ${size}`);
 }
 
-const optionsShort = { delay: 0, maxTime: 1, initCount: 0 };
-const optionsMedium = { delay: 0, maxTime: 2, initCount: 0 };
-const optionsLong = { delay: 0, maxTime: 10, initCount: 0 };
-
-function getTraditionalUploadCases(sizes: string[]) {
-  return sizes.map((size) =>
-    benny.add(
-      `traditional upload ${size}`,
-      () => {
-        const file = getFileOfSize(sizeToBytes(size));
-        return async () =>
-          await traditionalUpload(`traditional-upload-${size}`, file);
-      },
-      optionsMedium
-    )
-  );
-}
-
 const measures: Record<string, number[]> = {};
 
-function getMultipartUploadCases(sizes: string[], chunks: string[]) {
-  return sizes.flatMap((size) =>
-    chunks.map((chunk) =>
-      benny.add(
-        `multipart upload ${size} with ${chunk} chunks`,
-        () => {
-          const chunkData = getFileOfSize(sizeToBytes(chunk));
-          const lastChunkData = getFileOfSize(
-            sizeToBytes(size) % sizeToBytes(chunk)
-          );
-
-          const uploadMeasure = `upload-multipart-upload-${size}-${chunk}`;
-          const completeMeasure = `complete-multipart-upload-${size}-${chunk}`;
-          measures[uploadMeasure] = [];
-          measures[completeMeasure] = [];
-
-          return async () => {
-            let remainingBytes = sizeToBytes(size);
-            const chunkSize = sizeToBytes(chunk);
-
-            let start = Date.now();
-            let end = Date.now();
-
-            const { UploadId, Key } = await initiateMultipart(
-              `multipart-upload-${size}-${chunk}`
-            );
-
-            if (!UploadId || !Key) {
-              throw new Error('UploadId or Key is undefined');
-            }
-
-            const eTags: string[] = [];
-            let partNumber = 1;
-
-            start = Date.now();
-            while (remainingBytes > chunkSize) {
-              eTags.push(
-                await multipartUpload(UploadId, Key, partNumber, chunkData)
-              );
-
-              partNumber++;
-              remainingBytes -= sizeToBytes(chunk);
-            }
-
-            if (remainingBytes > 0) {
-              eTags.push(
-                await multipartUpload(UploadId, Key, partNumber, lastChunkData)
-              );
-            }
-            end = Date.now();
-            measures[uploadMeasure].push(end - start);
-
-            start = Date.now();
-            await completeMultipart(UploadId, Key, eTags);
-            end = Date.now();
-
-            measures[completeMeasure].push(end - start);
-          };
-        },
-        sizeToBytes(size) >= sizeToBytes('1g') ? optionsLong : optionsShort
-      )
-    )
-  );
-}
-
-async function run() {
-  console.log(`Creating bucket '${Bucket}'`);
-
-  await s3.send(new CreateBucketCommand({ Bucket }));
-
-  await benny.suite(
-    'Test',
-    benny.add(
-      'get traditional presigned URL only',
-      () => getTraditionalPresignedUrl('traditional-presigned-url-only'),
-      optionsShort
-    ),
-    benny.add(
-      'initiate multipart upload only',
-      () => initiateMultipart('initiate-multipart-only'),
-      optionsShort
-    ),
-    benny.add(
-      'get multipart presigned URL only',
-      async () => {
-        const { Key, UploadId } = await initiateMultipart(
-          'multipart-presigned-url-only'
-        );
-        if (!Key || !UploadId) {
-          throw new Error('Key or UploadId is undefined');
-        }
-
-        return () => getMultipartPresignedUrl(Key, UploadId, 1);
-      },
-      optionsShort
-    ),
-
-    ...getTraditionalUploadCases([
-      '512k',
-      '5m',
-      '10m',
-      '25m',
-      '50m',
-      '100m',
-      '1g',
-      '1.5g',
-      // got OS/syscall errors attempting 2g :(
-    ]),
-
-    ...getMultipartUploadCases(
-      ['512k', '5m', '10m', '25m', '50m', '100m', '1g', '2g', '5g'],
-      ['5m', '25m', '50m', '100m']
-    ),
-
-    benny.cycle((result) =>
-      console.log(
-        `${result.name}: ${result.details.mean.toFixed(
-          3
-        )} ± ${result.details.marginOfError.toFixed(3)} seconds/op (${
-          result.samples
-        } samples)`
-      )
-    ),
-    benny.complete(() => ({})),
-    benny.save({ file: 'result', details: true })
-  );
-
-  console.log('Saving supplemental multipart data...');
-  Object.values(measures).forEach((measure) => measure.unshift());
-  await promisify(writeFile)(
-    'benchmark/results/multipart-detailed.json',
-    JSON.stringify(measures)
-  );
-
-  console.log('Deleting temporary files...');
+async function cleanup() {
   let Objects =
     (
       await s3.send(
@@ -304,6 +152,195 @@ async function run() {
         )
       ).Contents?.map(({ Key }) => ({ Key })) ?? [];
   }
+}
+
+async function measure<T>(
+  name: string,
+  run:
+    | {
+        setup: () => T;
+        fn: (res: Awaited<T>) => Promise<unknown> | unknown;
+      }
+    | {
+        fn: () => Promise<unknown> | unknown;
+      },
+  maxTimeMs: number
+) {
+  measures[name] = measures[name] ?? [];
+
+  let overallStart = Date.now();
+
+  process.stdout.write(`${name}: starting`);
+
+  let res: Awaited<T> | undefined = undefined;
+  if ('setup' in run) {
+    res = await run.setup();
+  }
+
+  while (measures[name].length < 3 || Date.now() - overallStart < maxTimeMs) {
+    const start = Date.now();
+    if ('setup' in run) {
+      await run.fn(res as Awaited<T>);
+    } else {
+      await run.fn();
+    }
+    const end = Date.now();
+
+    measures[name].push(end - start);
+    process.stdout.clearLine(0);
+    process.stdout.cursorTo(0);
+    process.stdout.write(
+      `${name}: ${measures[name].length} samples, last ${end - start}ms`
+    );
+  }
+
+  process.stdout.clearLine(0);
+  process.stdout.cursorTo(0);
+  console.log(
+    `${name}: approx ${(
+      measures[name].reduce((a, b) => a + b, 0) / measures[name].length
+    ).toFixed(2)}ms (${measures[name].length} samples)`
+  );
+
+  await cleanup();
+  await promisify(writeFile)('results.json', JSON.stringify(measures));
+}
+
+async function measureTraditionalUploadCases(sizes: string[]) {
+  for (const size of sizes) {
+    await measure(
+      `traditional-upload-${size}`,
+      {
+        setup: () => getFileOfSize(sizeToBytes(size)),
+        fn: (file) => traditionalUpload(`traditional-upload-${size}`, file),
+      },
+      2000
+    );
+  }
+}
+
+async function measureMultipartUploadCases(sizes: string[], chunks: string[]) {
+  for (const size of sizes) {
+    for (const chunk of chunks) {
+      await measure(
+        `upload-multipart-${size}-${chunk}`,
+        {
+          setup: () => {
+            const chunkData = getFileOfSize(sizeToBytes(chunk));
+            const lastChunkData = getFileOfSize(
+              sizeToBytes(size) % sizeToBytes(chunk)
+            );
+
+            const uploadMeasure = `upload-multipart-upload-${size}-${chunk}`;
+            const completeMeasure = `upload-multipart-complete-${size}-${chunk}`;
+            measures[uploadMeasure] = [];
+            measures[completeMeasure] = [];
+            return { chunkData, lastChunkData, uploadMeasure, completeMeasure };
+          },
+          fn: async ({
+            chunkData,
+            lastChunkData,
+            uploadMeasure,
+            completeMeasure,
+          }) => {
+            let remainingBytes = sizeToBytes(size);
+            const chunkSize = sizeToBytes(chunk);
+
+            const { UploadId, Key } = await initiateMultipart(
+              `multipart-upload-${size}-${chunk}`
+            );
+
+            if (!UploadId || !Key) {
+              throw new Error('UploadId or Key is undefined');
+            }
+
+            const eTags: string[] = [];
+            let partNumber = 1;
+
+            let start = Date.now();
+            while (remainingBytes > chunkSize) {
+              eTags.push(
+                await multipartUpload(UploadId, Key, partNumber, chunkData)
+              );
+
+              partNumber++;
+              remainingBytes -= sizeToBytes(chunk);
+            }
+
+            if (remainingBytes > 0) {
+              eTags.push(
+                await multipartUpload(UploadId, Key, partNumber, lastChunkData)
+              );
+            }
+            let end = Date.now();
+            measures[uploadMeasure].push(end - start);
+
+            start = Date.now();
+            await completeMultipart(UploadId, Key, eTags);
+            end = Date.now();
+
+            measures[completeMeasure].push(end - start);
+          },
+        },
+        sizeToBytes(size) >= sizeToBytes('1g') ? 10000 : 2000
+      );
+    }
+  }
+}
+
+async function run() {
+  console.log(`Creating bucket '${Bucket}'`);
+
+  await s3.send(new CreateBucketCommand({ Bucket }));
+
+  // await measure(
+  //   'get-traditional-presigned-url',
+  //   { fn: () => getTraditionalPresignedUrl('traditional-presigned-url-only') },
+  //   1000
+  // );
+  // await measure(
+  //   'initiate-multipart-only',
+  //   { fn: () => initiateMultipart('initiate-multipart-only') },
+  //   1000
+  // );
+  // await measure(
+  //   'get-multipart-presigned-url-only',
+  //   {
+  //     setup: async () => {
+  //       const { Key, UploadId } = await initiateMultipart(
+  //         'multipart-presigned-url-only'
+  //       );
+  //       if (!Key || !UploadId) {
+  //         throw new Error('Key or UploadId is undefined');
+  //       }
+  //       return { Key, UploadId };
+  //     },
+  //     fn: ({ Key, UploadId }) => getMultipartPresignedUrl(Key, UploadId, 1),
+  //   },
+  //   1000
+  // );
+  // await measureTraditionalUploadCases([
+  //   '512k',
+  //   '5m',
+  //   '10m',
+  //   '25m',
+  //   '50m',
+  //   '100m',
+  //   '1g',
+  //   '1.5g',
+  //   // got OS/syscall errors attempting 2g :(
+  // ]);
+  // await measureMultipartUploadCases(
+  //   ['512k', '5m', '10m', '25m', '50m', '100m'],
+  //   ['5m', '25m', '50m', '100m']
+  // );
+  await measureMultipartUploadCases(
+    ['1g', '2g', '5g'],
+    [/* '5m', '25m', '50m', '100m',  */ '200m', '500m']
+  );
+
+  console.log('Deleting temporary S3 files...');
+  await cleanup();
 
   console.log(`Deleting bucket '${Bucket}'`);
   await s3.send(new DeleteBucketCommand({ Bucket }));
